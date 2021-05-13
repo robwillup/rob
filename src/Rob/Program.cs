066f8e71cda32d7ce6f5f71e8c6be47ca9342d3a
Robson William <r.willup@hotmail.com>
2021-05-13 16:09:31 -0300

    Renamed project

--- src/Rob/Program.cs
+++ src/Rob/Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace RobsonWilliam
+namespace Rob
 {
     public class Program
     {
