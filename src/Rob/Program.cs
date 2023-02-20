ea26afe529be9979e0e41bb616014e5cc13de4bf
Robson William <robson.dasilva@plurilock.com>
2023-02-20 11:42:10 -0800

    Blazor web site running again

--- src/Rob/Program.cs
+++ src/Rob/Program.cs
@@ -1,6 +1,7 @@
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
--- src/Rob/Usings.cs
+++ src/Rob/Usings.cs
@@ -1,2 +1,3 @@
-﻿global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 global using Rob;
