c2e322f7c87d6c9114e0fcbb3a498ccdad984821
Robson William <robson.dasilva@plurilock.com>
2023-03-04 22:00:33 -0800

    Removed MongoDB dependency

--- src/Rob.Api/Program.cs
+++ src/Rob.Api/Program.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
-using Rob.Api.Mongo;
-using System;
-using System.Net;
-using Rob.Api.Models;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
@@ -19,38 +14,20 @@ app.UseCors();
 
 app.MapGet("/articles", (async contex =>
 {
-    var dbConn = new DbConnector(app.Configuration["ConnStr"]);
-    List<Article> articles = await dbConn.GetAllDocs();
-    await contex.Response.WriteAsJsonAsync(articles);
+    await contex.Response.WriteAsync("Coming soon");
 }));
 
 app.MapGet("/articles/{id}", (async context =>
 {
-    var dbConn = new DbConnector(app.Configuration["ConnStr"]);
-    string id = context.Request.RouteValues["id"].ToString();
-    Article result = await dbConn.GetOneDocByTitleAsync(id);
-    await context.Response.WriteAsJsonAsync(result);
+    await context.Response.WriteAsync("coming soon");
 })).RequireCors(op => 
 {
     op.AllowAnyOrigin();
 });
 
-app.MapPost("/articles", async context =>
-{    
-    if(!context.Request.HasJsonContentType())
-    {
-        context.Response.StatusCode = (int) HttpStatusCode.UnsupportedMediaType;
-        return;
-    }
-    var post = await context.Request.ReadFromJsonAsync<Article>();
-    var dbConn = new DbConnector(app.Configuration["ConnStr"]);
-    await dbConn.InsertOneDocAsync(post);
-    context.Response.StatusCode = (int) HttpStatusCode.Accepted;
-});
-
-app.MapGet("/", (Func<string>)( () =>
+app.MapGet("/", () =>
 {
     return "\"For even the very wise cannot see all ends.\" -- Gandalf";
-}));
+});
 
 await app.RunAsync();
--- src/Rob/Models/PostContent.cs
+++ src/Rob/Models/PostContent.cs
@@ -1,5 +1,4 @@
 using System;
-using MongoDB.Bson;
 
 namespace Rob.Models
 {
