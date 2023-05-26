3a9e2c51e62f4774164b89c278d806d50ebe687a
Robson William <r.willup@hotmail.com>
2023-05-26 12:32:51 -0700

    Restoring PAT that GitGuardian deleted without asking if I even wanted its help.

--- src/Rob/Services/PostManager.cs
+++ src/Rob/Services/PostManager.cs
@@ -10,7 +10,7 @@ namespace Rob.Services
         {
             Uri uri = new("https://api.github.com/repos/robwillup/mithrandir/contents/docs");
             HttpClient http = new();
-            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
             http.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
             http.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
 
