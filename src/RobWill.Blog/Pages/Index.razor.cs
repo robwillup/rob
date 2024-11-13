2a00fd647d934480853c364c6a4535427905c867
Robson William <30180338+robwillup@users.noreply.github.com>
2024-11-12 22:11:58 -0300

    New content

--- src/RobWill.Blog/Pages/Index.razor.cs
+++ src/RobWill.Blog/Pages/Index.razor.cs
@@ -7,10 +7,4 @@ public partial class Index : IComponent
 {
     [Inject]
     protected IBlogService? BlogService {get;set;}
-    private List<GitHubItem>? _items;
-
-    protected override async Task OnInitializedAsync()
-    {
-        _items = await BlogService.GetBlogPostsAsync();
-    }
 }
